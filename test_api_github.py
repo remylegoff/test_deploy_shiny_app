7571be4538cd2ba48dd4d57cdb58028c507d4502
Rémy LE GOFF <105215913+remylegoff@users.noreply.github.com>
2024-09-16 14:40:58 +0200

    Add files via upload

---
+++ test_api_github.py
@@ -0,0 +1,36 @@
+from github import Github
+import os
+# Authentication is defined via github.Auth
+from github import Auth
+
+# using an access token
+auth = Auth.Token(os.getenv("GITHUB_TOKEN"))
+
+# First create a Github instance:
+
+# Public Web Github
+g = Github(auth=auth)
+
+# Github Enterprise with custom hostname
+g = Github(base_url="https://github.com/remylegoff/", auth=auth)
+os.chdir("test_chimstat")
+# Then play with your Github objects:
+repo = g.get_user().get_repo("ChimstatxMWine")
+contents = repo.get_contents("")
+os.mkdir("Chimstat")
+while contents:
+    file_content = contents.pop(0)
+    if file_content.type == "dir":
+        print(file_content.path)
+        if file_content.path not in os.listdir():
+            print(file_content.path)
+            os.mkdir(file_content.path)
+        contents.extend(repo.get_contents(file_content.path))
+    else:
+        print(file_content.path)
+        with open(file_content.path, "wb") as f:
+            f.write(file_content.decoded_content)
+# To close connections after use
+g.close()
+
+os.system("python3 Chimstat/app.py")
